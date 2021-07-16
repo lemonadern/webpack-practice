c1db4cb09a3441cb00e191deec920cf6e20bf788
lemonadern <taishi.develop@gmail.com>
2021-07-17 00:31:35 +0900

    change dom to DOM

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
 
-ReactDom.render(
+ReactDOM.render(
     <React.StrictMode>
         <p>HI</p>
     </React.StrictMode>,
