00a885234b8506988272432307c3e92bb6d6eea6
lemonadern <taishi.develop@gmail.com>
2021-07-17 00:09:57 +0900

    Reactアプリケーションを追加

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+ReactDom.render(
+    <React.StrictMode>
+        <p>HI</p>
+    </React.StrictMode>,
+    document.getElementById('app')
+)
