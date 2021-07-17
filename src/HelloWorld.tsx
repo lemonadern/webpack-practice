976567461eb9813ca90db95cdb4e1a153573b1f1
lemonadern <taishi.develop@gmail.com>
2021-07-17 19:10:28 +0900

    ホットリロードが効くのを確認

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -4,6 +4,7 @@ const HelloWorld: React.FC = () => {
     return (
         <div>
             <h1>Hello World!!!</h1>
+            <p>It's your React app. Happy Hacking!!!</p>
         </div>
     )
 }
