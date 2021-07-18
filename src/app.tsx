68533bc454d1f8886898364dcfdb4e4a33022835
lemonadern <taishi.develop@gmail.com>
2021-07-18 14:35:40 +0900

    Appコンポーネントとして追加

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom'
 
 import HelloWorld from './HelloWorld'
 
+const App: React.FC = () => {
+    return(
+        <div>
+            <HelloWorld />
+        </div>
+    )
+}
 
 ReactDOM.render(
     <React.StrictMode>
-        <HelloWorld />
+        <App />
     </React.StrictMode>,
     document.getElementById('app')
 )
